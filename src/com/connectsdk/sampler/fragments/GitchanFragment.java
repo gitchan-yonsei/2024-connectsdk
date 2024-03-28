2d67ab72a16ecc420839099b5848745676f5c572
gitchan <eunkeeeee@gmail.com>
2024-03-28 16:45:33 +0900

    feat: enable buttons of gitchan fragment

--- src/com/connectsdk/sampler/fragments/GitchanFragment.java
+++ src/com/connectsdk/sampler/fragments/GitchanFragment.java
@@ -14,17 +14,14 @@ import android.widget.SeekBar;
 import com.connectsdk.sampler.R;
 import com.connectsdk.sampler.util.TestResponseObject;
 import com.connectsdk.sampler.widget.AppAdapter;
+import com.connectsdk.service.capability.VolumeControl;
 
 public class GitchanFragment extends BaseFragment {
 
-    public Button youtubeButton;
-    public Button netflixButton;
     public Button playButton;
     public Button pauseButton;
     public Button stopButton;
     public SeekBar mVolumeBar;
-    public ListView appListView;
-    public AppAdapter adapter;
     public Handler mHandler;
     public TestResponseObject testResponse;
 
@@ -51,7 +48,6 @@ public class GitchanFragment extends BaseFragment {
         stopButton = (Button) rootView.findViewById(R.id.stopButton);
         mVolumeBar = (SeekBar) rootView.findViewById(R.id.volume_seek_bar);
 
-
         buttons = new Button[3];
         buttons[0] = playButton;
         buttons[1] = pauseButton;
@@ -60,5 +56,47 @@ public class GitchanFragment extends BaseFragment {
         mHandler = new Handler();
 
         return rootView;
+    }
+
+    @Override
+    public void enableButtons() {
+        playButton.setOnClickListener(playClickListener);
+        pauseButton.setOnClickListener(pauseClickListener);
+        stopButton.setOnClickListener(stopClickListener);
+        mVolumeBar.setEnabled(getTv().hasCapability(VolumeControl.Volume_Set));
+        mVolumeBar.setOnSeekBarChangeListener(volumeSeekListener);
+        super.enableButtons();
+    }
+
+    private View.OnClickListener playClickListener = view -> getMediaControl().play(null);
+
+    private View.OnClickListener pauseClickListener = view -> getMediaControl().pause(null);
+
+    private View.OnClickListener stopClickListener = view -> getMediaControl().stop(null);
+
+    private SeekBar.OnSeekBarChangeListener volumeSeekListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            if (fromUser) {
+                float fVol = (float) (progress / 100.0);
+                getVolumeControl().setVolume(fVol, null);
+            }
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+        }
+    };
+
+    @Override
+    public void disableButtons() {
+        mVolumeBar.setEnabled(false);
+        mVolumeBar.setOnSeekBarChangeListener(null);
+
+        super.disableButtons();
     }
 }
